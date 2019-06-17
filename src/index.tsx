76425a4d70fd6ce54fa5400172cc8ad1a4b75230
Zack Kollar <rallokkcaz@gmail.com>
2019-06-17 03:16:20 -0700

    Tested hooks properly

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,13 @@
-import * as React from "react";
+import React, { useCallback, useState } from "react";
+
+export const useCounter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = useCallback(() => setCount(x => x + 1), []);
+  const decrement = useCallback(() => setCount(x => x - 1), []);
+
+  return { count, increment, decrement };
+};
 
 // Delete me
 export const Thing = () => {
--- test/initial.test.tsx
+++ test/initial.test.tsx
@@ -1,19 +1,35 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { renderHook, act } from "react-hooks-testing-library";
+import { render, waitForElement } from "@testing-library/react";
 
-import { render, waitForElement } from '@testing-library/react';
+import { Thing, useCounter } from "../src";
 
-import { Thing } from '../src';
-
-describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("it", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Thing />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('can read the components innerText correctly', async () => {
+  it("can read the components innerText correctly", async () => {
     const { getByText } = await render(<Thing />);
     await waitForElement(() => getByText(/i work/i));
+  });
+
+  it("should increment counter", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => result.current.increment());
+
+    expect(result.current.count).toBe(1);
+  });
+
+  it("should decrement counter", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => result.current.decrement());
+
+    expect(result.current.count).toBe(-1);
   });
 });
